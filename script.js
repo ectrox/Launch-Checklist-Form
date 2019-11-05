329154ffaeb78e7f4ffccbbf2614918fc4fe2c03
Emma Troxell <troxellemma@gmail.com>
2019-11-05 14:22:03 -0600

    added preventDefault to form

--- script.js
+++ script.js
@@ -11,3 +11,16 @@
 </ol>
 <img src="${}">
 */
+const pilotName = document.getElementById('pilotName');
+const copilotName = document.getElementById('copilotName');
+const fuelLevel = document.getElementById('fuelLevel');
+const cargoMass = document.getElementById('cargoMass');
+const formSubmit = document.getElementById('formSubmit');
+
+formSubmit.addEventListener('click',function(event) {
+   if (pilotName.value ==='' || copilotName.value==='' || fuelLevel.value==='' || cargoMass.value==='') {
+      alert('All field are required!');
+      event.preventDefault();
+   }
+});
+console.log(pilotName.value);
