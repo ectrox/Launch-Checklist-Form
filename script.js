dfb0fdf627f7434bd6c14c04ccf91e005c87cf9c
Emma Troxell <troxellemma@gmail.com>
2019-11-06 13:34:30 -0600

    adding validation

--- script.js
+++ script.js
@@ -17,9 +17,25 @@ const fuelLevel = document.getElementById('fuelLevel');
 const cargoMass = document.getElementById('cargoMass');
 const formSubmit = document.getElementById('formSubmit');
 
+
+
 formSubmit.addEventListener('click',function(event) {
+   
+   
+   function isValid() {
+      let invalid = ['1','2','3','4','5','6','7','8','9','0'];
+   
+   };
    if (pilotName.value ==='' || copilotName.value==='' || fuelLevel.value==='' || cargoMass.value==='') {
       alert('All field are required!');
       event.preventDefault();
-   }
+   } if (isNaN(fuelLevel.value) || isNaN(cargoMass.value)) {
+      alert('Please enter a valid number');
+      event.preventDefault();
+   } /*if (pilotName.value || copilotName.value ) {
+      alert('Please enter a valid name');
+      event.preventDefault();
+   } */
 });
+
+
